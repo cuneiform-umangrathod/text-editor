a16cb9f35ee30e7a9c9df0a12ad190b6a6f5f051
cuneiform-umangrathod <umang.rathod@thecuneiform.com>
2023-10-13 13:14:50 +0530

    text editor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text , setText] = useState({name:"",value:""});
+  const handleChange = (e)=>{
+    let name = e.target.name
+    let value= e.target.value
+    setText({...text,name:name,value:value});
+    console.log(text);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="editor-body">
+          <div className="tools">
+            <button className="bold">B</button>
+            <button className="itelic">I</button>
+            <button className="strick">S</button>
+            <button className="Underline">U</button>
+            <button className="Mark">M</button>
+            <button className="undo">undo</button>
+            <button className="redo">redo</button>
+          </div>
+          <div className="text-editor">
+
+            <textarea name="textarea" cols="30" rows="10" onChange={handleChange}> 
+{text.value} 
+            </textarea>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
